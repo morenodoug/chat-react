04de2f9815d1861cb7d9f3e019c1c9c19c9c71eb
Douglas Moreno <morenodoug@gmail.com>
2017-08-02 11:45:39 -0400

    react-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import {FaUser} from 'react-icons/lib/fa';
+
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,6 +20,24 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="row">  
+          <div className="col-md-4 col-md-offset-4">
+            <div className="form-top" >
+              <div className="left">
+                <h4>Registro   </h4>
+              </div>
+              <div className="right">
+                <FaUser size={30} className="icon"/>
+              </div>
+            
+            </div>
+            <div className="form-bottom"></div>
+
+         
+
+          
+          </div>        
+        </div>
       </div>
     );
   }
